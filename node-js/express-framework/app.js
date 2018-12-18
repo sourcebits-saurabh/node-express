de8b99062b3b9d002c1f09cb3f1e8e2139787ac9
Saurabh Kamble <saurabhkamble@SBMAC0273.local>
2018-12-18 17:33:07 +0530

    working on express

--- node-js/express-framework/app.js
+++ node-js/express-framework/app.js
@@ -1,10 +1,19 @@
 
 const express = require('express');
-
-
+const path = require('path');
 const app = express();
 
+app.use('/public', express.static(path.join(__dirname, 'static')))
+
 app.get('/',(req,res)=>{
-    res.send("Happy to use express");
+    res.sendFile(path.join(__dirname,'static','index.html'))
+});
+
+app.get('/example',(req,res)=>{
+    res.send("fetching for exmaple in express");
+});
+app.get('/example/:name/:age',(req,res)=>{
+    console.log(req.query);
+    res.send(req.params.name+":"+req.params.age);
 });
 app.listen("4000");
