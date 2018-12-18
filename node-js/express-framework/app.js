7a19fb591267bb5fb6abf15fd6f5a97e74ff7dc0
Saurabh Kamble <saurabhkamble@SBMAC0273.local>
2018-12-18 18:59:51 +0530

    changes in express

--- node-js/express-framework/app.js
+++ node-js/express-framework/app.js
@@ -1,16 +1,44 @@
 
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const Joi = require('joi');
 const app = express();
 
 app.use('/public', express.static(path.join(__dirname, 'static')))
-
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'static','index.html'))
 });
 
 app.get('/example',(req,res)=>{
     res.send("fetching for exmaple in express");
+});
+
+app.post('/',(req,res)=>{
+    console.log(req.body);
+    //database working here
+
+    // res.send('SuccessFully posted data');
+
+    const schema = Joi.object().keys({
+        email:Joi.string().trim().email().required(),
+        password:Joi.string().min(5).max(10).required(),
+    });
+
+    Joi.validate(req.body,schema,(err,result)=>{
+
+        if(err){
+            console.log(err);
+            res.send('An error occured');}
+        else
+        {
+            console.log(result);
+            res.send('success in validation of form data');
+        }
+    });
+    res.json({success:true})
 });
 app.get('/example/:name/:age',(req,res)=>{
     console.log(req.query);
